f95f269e0d0b36f828c976c51fdf305e5f83f6db
Son <hathanhson2706@gmail.com>
2021-01-07 21:29:58 +0100

    Zadanie 1 - 59

--- 59/zadanie.py
+++ 59/zadanie.py
@@ -1,3 +1,24 @@
+def czynniki(liczba):
+    ile = 0
+    czynnik = 3
+    if liczba % 2 == 0:
+        return False
+    while liczba > 1:
+        if liczba % czynnik == 0:
+            ile += 1
+        while liczba % czynnik == 0:
+            liczba = liczba/czynnik
+
+        czynnik = czynnik+2
+        if ile > 3:
+            return False
+
+    if ile == 3:
+        return True
+    if ile < 3:
+        return False
+
+
 if __name__ == '__main__':
 
     liczby = []
@@ -5,4 +26,10 @@ if __name__ == '__main__':
         for line in f:
             liczby.append(int(line))
 
-    print(liczby)
+    counter = 0
+    for liczba in liczby:
+        print(czynniki(liczba))
+        if czynniki(liczba):
+            counter += 1
+
+    print("Zadanie1. {}".format(counter))
