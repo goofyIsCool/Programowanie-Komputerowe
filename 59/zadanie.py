b211b31ad41b4d943d6bcc3043b06f3e07897226
Son <hathanhson2706@gmail.com>
2021-01-07 22:06:18 +0100

    Zadanie 2 - 59

--- 59/zadanie.py
+++ 59/zadanie.py
@@ -19,6 +19,27 @@ def czynniki(liczba):
         return False
 
 
+def palindrom(liczba):
+    s = str(liczba)
+
+    for i in range(len(s)):
+        if (s[i] != s[len(s)-1-i]):
+            return False
+
+    return True
+
+
+def palindromSum(liczba):
+    tmp = str(liczba)
+    liczbaReversed = int(tmp[::-1])
+
+    sum = liczba + liczbaReversed
+    if palindrom(sum):
+        return True
+    else:
+        return False
+
+
 if __name__ == '__main__':
 
     liczby = []
@@ -26,10 +47,16 @@ if __name__ == '__main__':
         for line in f:
             liczby.append(int(line))
 
-    counter = 0
+    counter1 = 0
+    counter2 = 0
     for liczba in liczby:
-        print(czynniki(liczba))
-        if czynniki(liczba):
-            counter += 1
+        # print(palindromSum(liczba))
+        # if czynniki(liczba):
+        #     counter1 += 1
+        if palindromSum(liczba):
+            counter2 += 1
 
-    print("Zadanie1. {}".format(counter))
+    # print("Zadanie1. {}".format(counter))
+    # 114
+    print("Zadnaie2. {}".format(counter2))
+    # 181
